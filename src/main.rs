21a0a2fddc4c2c7cea6b8943378d165dea8300c1
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-01-06 21:26:43 +0300

    fuss

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ use harfbuzz_rs as hb;
 use imageproc::drawing::Canvas as _;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path =
-        "/Users/abdulrahmansibahi/Documents/rust/mimiron/mimiron/fonts/YanoneKaffeesatz-Medium.ttf";
+    // let path = "YanoneKaffeesatz-Medium.ttf";
+    let path = "NotoNaskhArabic-Bold.ttf";
+    // let path = "Gulzar-Regular.ttf";
+
+    let buffer = hb::UnicodeBuffer::new().add_str("Hêllo World!");
+    // let buffer = hb::UnicodeBuffer::new().add_str("أهلا بالعالم");
+
     let font_data = std::fs::read(path)?;
 
     let index = 0; //< face index in the font file
     let hb_font = hb::Font::new(hb::Face::from_bytes(&font_data, index));
 
-    let buffer = hb::UnicodeBuffer::new().add_str("Hêllo World!");
     let output = hb::shape(&hb_font, buffer, &[]);
 
     let positions = output.get_glyph_positions();
@@ -20,7 +24,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let ab_font = ab::FontRef::try_from_slice(&font_data)?;
 
-    let width = 300;
+    let width = 1000;
     let height = 300;
 
     let mut canvas: image::RgbaImage = image::ImageBuffer::new(width, height);
@@ -44,25 +48,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             gid, cluster, x_advance, x_offset, y_offset
         );
 
-        let ab_scale = ab_font.pt_to_px_scale(60.0).unwrap().x;
-        let hb_scale = hb_font.scale().0;
+        let ab_scale = ab_font.pt_to_px_scale(60.0).unwrap();
+        let hb_scale = hb_font.scale();
 
-        let horizontal = ab_scale * (caret + x_offset) as f32 / hb_scale as f32;
-        let vertical =
-            ab_font.as_scaled(ab_scale).ascent() + (ab_scale * y_offset as f32 / hb_scale as f32);
+        let horizontal = ab_scale.x * (caret + x_offset) as f32 / hb_scale.0 as f32;
+        let vertical = ab_scale.y * y_offset as f32 / hb_scale.1 as f32 +
+            // to push the line down.
+            ab_font.as_scaled(ab_scale).ascent();
 
-        let gl =
-            ab_glyph::GlyphId(gid as u16).with_scale_and_position(ab_scale, (horizontal, vertical));
+        let gl = ab_glyph::GlyphId(gid as u16)
+            .with_scale_and_position(ab_scale, ab_glyph::point(horizontal, vertical));
 
-        let Some(y) = ab_font.outline_glyph(gl) else {
+        let Some(outlined_glyph) = ab_font.outline_glyph(gl) else {
             // gl is whitespace
             caret += x_advance;
             continue;
         };
 
-        let bb = y.px_bounds();
+        let bb = outlined_glyph.px_bounds();
 
-        y.draw(|px, py, pv| {
+        // USELESS BLUE BOUNDING BOX
+        for x in bb.min.x as u32..=bb.max.x as u32 {
+            canvas.draw_pixel(x + 15, bb.min.y as u32 + 15, image::Rgba([0, 0, 255, 255]));
+            canvas.draw_pixel(x + 15, bb.max.y as u32 + 15, image::Rgba([0, 0, 255, 255]));
+        }
+        for y in bb.min.y as u32..=bb.max.y as u32 {
+            canvas.draw_pixel(bb.min.x as u32 + 15, y + 15, image::Rgba([0, 0, 255, 255]));
+            canvas.draw_pixel(bb.max.x as u32 + 15, y + 15, image::Rgba([0, 0, 255, 255]));
+        }
+
+        outlined_glyph.draw(|px, py, pv| {
             let px = px as f32 + bb.min.x + 15.0;
             let py = py as f32 + bb.min.y + 15.0;
 
