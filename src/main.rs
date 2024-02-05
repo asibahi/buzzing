9976f9769a361461b5ba58864096055cff0526a9
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-02-05 20:42:01 +0300

    adaptable width

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let index = 0; //< face index in the font file
     let mut hb_font = hb::Font::new(hb::Face::from_bytes(&font_data, index));
-    
+
     hb_font.set_variations(&[hb::Variation::new(b"wdth", 100.0)]); // variable font
 
     let output = hb::shape(&hb_font, buffer, &[]);
@@ -27,17 +27,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let positions = output.get_glyph_positions();
     let infos = output.get_glyph_infos();
 
+    let width: i32 = positions.iter().map(|gp| gp.x_advance).sum();
+
     let ab_font = ab::FontRef::try_from_slice(&font_data)?;
-
-    let width = 1000;
-    let height = 300;
-
-    let mut canvas: image::RgbaImage = image::ImageBuffer::new(width, height);
-    imageproc::drawing::draw_filled_rect_mut(
-        &mut canvas,
-        imageproc::rect::Rect::at(0, 0).of_size(width, height),
-        image::Rgba([255, 255, 255, 255]),
-    );
 
     let mut caret = 0;
 
@@ -47,6 +39,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let scale_factor = ab_scaled_font.scale_factor();
 
     let ascent = ab_scaled_font.ascent();
+
+    let width = (width as f32 * scale_factor.horizontal) as u32 + 30;
+    dbg!(width);
+    let height = 300;
+
+    let mut canvas: image::RgbaImage = image::ImageBuffer::new(width, height);
+    imageproc::drawing::draw_filled_rect_mut(
+        &mut canvas,
+        imageproc::rect::Rect::at(0, 0).of_size(width, height),
+        image::Rgba([255, 255, 255, 255]),
+    );
 
     for (position, info) in positions.iter().zip(infos) {
         let gid = info.codepoint;
